dc08d0d6a0297479d79bc9981931df7cfe5009a5
Mohsin-khan540 <arhamk4555@gmail.com>
2025-11-05 13:03:45 +0500

    Initial Commit

---
+++ Remember Names/Remember_NamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remember_NamesApp.swift
+//  Remember Names
+//
+//  Created by Mohsin khan on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Remember_NamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Remember NamesTests/Remember_NamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Remember_NamesTests.swift
+//  Remember NamesTests
+//
+//  Created by Mohsin khan on 05/11/2025.
+//
+
+import Testing
+@testable import Remember_Names
+
+struct Remember_NamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
